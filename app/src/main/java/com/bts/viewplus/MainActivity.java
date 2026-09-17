926f2413ea62d691549208d0a969f8995dd17a17
notebook <flfrpdla2025@gmail.com>
2026-09-17 11:08:44 +0900

    Initial commit

---
+++ app/src/main/java/com/bts/viewplus/MainActivity.java
@@ -0,0 +1,29 @@
+package com.bts.viewplus;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.bts.viewplus.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
